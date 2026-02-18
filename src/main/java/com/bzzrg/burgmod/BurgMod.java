d24f48f3380c3b0f650ec945110ca126bf43a272
bzzrg <hamburgerdude0@gmail.com>
2026-02-18 14:58:21 -0500

    changed to version 1.1.1

--- src/main/java/com/bzzrg/burgmod/BurgMod.java
+++ src/main/java/com/bzzrg/burgmod/BurgMod.java
@@ -32,7 +32,7 @@ public class BurgMod {
 
     public static final String MODID = "burgmod";
     public static final String MODNAME = "BurgMod";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
 
     public static Minecraft mc;
     public static Logger logger;
